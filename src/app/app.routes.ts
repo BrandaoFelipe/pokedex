cf677361f4fc4460682ef4ff174dcb8c1f9f0147
Felipe Brandao <brandao154@outlook.com>
2025-06-16 11:54:30 -0300

    pagina inicial com lista de pokemon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'pokemon/:id',
+    loadComponent: () => import('./pages/pokemon-details.page').then(m => m.PokemonDetailsPage)
+  },
 ];
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, RouterModule],
+  
 })
-export class Tab1Page {
-  constructor() {}
+export class Tab1Page implements OnInit {
+  pokemons: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<any>('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
+      .subscribe((response) => {
+        this.pokemons = response.results.map((poke: any) => {
+          const id = poke.url.split('/')[6];
+          return {
+            name: poke.name,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+          };
+        });
+      });
+  }
 }
